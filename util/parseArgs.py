2d1b62ac4da0c319bf029c03cb6bcb46b840ac7f
yang_starry_sky <yangcd@bupt.edu.cn>
2022-05-04 00:18:22 +0800

    update model and others

--- util/parseArgs.py
+++ util/parseArgs.py
@@ -6,6 +6,7 @@ def parseArgs():
     parser.add_argument("--clear", type=str, default="clear")
     parser.add_argument("--hazy", type=str, default="hazy")
     parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--image_size", type=int, default=256)
     parser.add_argument("--g_a_path", type=str, default="generator_a.pkl")
     parser.add_argument("--g_b_path", type=str, default="generator_b.pkl")
     parser.add_argument("--d_a_path", type=str, default="discriminator_a.pkl")
